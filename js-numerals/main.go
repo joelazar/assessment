ecfd5bdb31e9047e361a28210f8af0e6db38fada
joelazar <jozsef.gabor.lazar@gmail.com>
2018-10-07 21:37:33 +0200

    two digit number conversion is ready

--- js-numerals/main.go
+++ js-numerals/main.go
@@ -6,21 +6,57 @@ import "strconv"
 
 var (
 	smallnumbers = map[int]string{
-		0: "zero",
-		1: "one",
-		2: "two",
-		3: "three",
-		4: "four",
-		5: "five",
-		6: "six",
-		7: "seven",
-		8: "eight",
-		9: "nine",
+		0:  "zero",
+		1:  "one",
+		2:  "two",
+		3:  "three",
+		4:  "four",
+		5:  "five",
+		6:  "six",
+		7:  "seven",
+		8:  "eight",
+		9:  "nine",
+		10: "ten",
+		11: "eleven",
+		12: "twelve",
+		13: "thirteen",
+		14: "fourteen",
+		15: "fifteen",
+		16: "sixteen",
+		17: "seventeen",
+		18: "eightteen",
+		19: "nineteen",
+	}
+
+	tens = map[int]string{
+		20: "twenty",
+		30: "thirty",
+		40: "forty",
+		50: "fifty",
+		60: "sixty",
+		70: "seventy",
+		80: "eighty",
+		90: "ninety",
 	}
 )
 
+func divmod(numerator, denominator int) (quotient, remainder int) {
+	quotient = numerator / denominator
+	remainder = numerator % denominator
+	return
+}
+
 func number_to_string(number int) string {
-	return smallnumbers[number]
+	if number > 19 {
+		_, remainder := divmod(number, 10)
+		if remainder != 0 {
+			return fmt.Sprintf("%s-%s", tens[number-remainder], smallnumbers[remainder])
+		} else {
+			return fmt.Sprintf("%s", tens[number-remainder])
+		}
+	} else {
+		return smallnumbers[number]
+	}
 }
 
 func main() {
--- js-numerals/main_test.go
+++ js-numerals/main_test.go
@@ -7,3 +7,15 @@ func TestOneDigit(t *testing.T) {
 		t.Error("Expected five, got ", result)
 	}
 }
+
+func TestTwoDigit(t *testing.T) {
+	if result := number_to_string(13); "thirteen" != result {
+		t.Error("Expected thirteen, got ", result)
+	}
+}
+
+func TestHigherTwoDigit(t *testing.T) {
+	if result := number_to_string(97); "ninety-seven" != result {
+		t.Error("Expected ninety-seven, got ", result)
+	}
+}
