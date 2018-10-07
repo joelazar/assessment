0795495b92ce3fe03eb6c21a9e265803439c5858
joelazar <jozsef.gabor.lazar@gmail.com>
2018-10-07 22:16:58 +0200

    refactored unit test cases

--- js-numerals/main_test.go
+++ js-numerals/main_test.go
@@ -2,44 +2,36 @@ package main
 
 import "testing"
 
-func TestOneDigit(t *testing.T) {
-	if result := number_to_string(5); "five" != result {
-		t.Error("Expected five, got ", result)
+func testNumberConverting(t *testing.T, number int, expected_string string) {
+	if result := number_to_string(number); expected_string != result {
+		t.Errorf("Expected %s, but got %s", expected_string, result)
 	}
+}
+
+func TestOneDigit(t *testing.T) {
+	testNumberConverting(t, 5, "five")
 }
 
 func TestTwoDigit(t *testing.T) {
-	if result := number_to_string(13); "thirteen" != result {
-		t.Error("Expected thirteen, got ", result)
-	}
+	testNumberConverting(t, 13, "thirteen")
 }
 
 func TestHigherTwoDigit(t *testing.T) {
-	if result := number_to_string(97); "ninety-seven" != result {
-		t.Error("Expected ninety-seven, got ", result)
-	}
+	testNumberConverting(t, 97, "ninety-seven")
 }
 
 func TestThreeDigit(t *testing.T) {
-	if result := number_to_string(101); "one hundred and one" != result {
-		t.Error("Expected one hundred and one, got ", result)
-	}
+	testNumberConverting(t, 101, "one hundred and one")
 }
 
 func TestHigherThreeDigit(t *testing.T) {
-	if result := number_to_string(982); "nine hundred and eighty-two" != result {
-		t.Error("Expected nine hundred and eighty-two, got ", result)
-	}
+	testNumberConverting(t, 982, "nine hundred and eighty-two")
 }
 
 func TestFourDigit(t *testing.T) {
-	if result := number_to_string(1001); "one thousand and one" != result {
-		t.Error("Expected one thousand and one, got ", result)
-	}
+	testNumberConverting(t, 1001, "one thousand and one")
 }
 
 func TestNineDigit(t *testing.T) {
-	if result := number_to_string(123456789); "one hundred and twenty-three million and four hundred and fifty-six thousand and seven hundred and eighty-nine" != result {
-		t.Error("Expected one hundred and twenty-three million and four hundred and fifty-six thousand and seven hundred and eighty-nine, got ", result)
-	}
+	testNumberConverting(t, 123456789, "one hundred and twenty-three million and four hundred and fifty-six thousand and seven hundred and eighty-nine")
 }
